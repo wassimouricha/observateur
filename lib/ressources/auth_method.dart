89a23a15d095b4300fb9f044c8ea233f220d725c
wassimouricha <wbouricha5@gmail.com>
2022-05-06 17:04:50 +0200

    sign up page and function added , image picker and storage added

---
+++ lib/ressources/auth_method.dart
@@ -0,0 +1,59 @@
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:observateur/ressources/storage_method.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // sign up user
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file,
+  }) async {
+    String res = "une erreur est arrivé";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty ||
+          file != null) {
+        // enregistrer l'utilisateur
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        // ignore: avoid_print
+        print(cred.user!.uid);
+
+        String photoUrl = await StorageMethods().uploadImageToStorage("profilePics", file, false);
+
+        // ajouter l'utilisateur a notre base de donnée
+        await _firestore.collection("users").doc(cred.user!.uid).set({
+          "username": username,
+          "uid": cred.user!.uid,
+          "email": email,
+          "bio": bio,
+          "followers": [],
+          "following": [],
+          "photoUrl" : photoUrl,
+        });
+
+        // //
+        // await _firestore.collection("users").add({
+
+        // });
+
+        res = "success";
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,7 +1,13 @@
+
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:observateur/ressources/auth_method.dart';
 import 'package:observateur/screens/login_screen.dart';
 
 import 'package:observateur/utils/colors.dart';
+import 'package:observateur/utils/utils.dart';
 import 'package:observateur/widget/text_field_rename.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -17,6 +23,7 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
+  Uint8List? _image;
 
   @override //créer un state object qui ne sera jamais build plus tard
   void dispose() {
@@ -27,140 +34,176 @@ class _SignupScreenState extends State<SignupScreen> {
     _usernameController.dispose();
   }
 
+  void selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-          width: double.infinity,
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Flexible(
-              child: Container(),
-              flex: 2,
-            ),
-            // image
-            Image.asset("image/observateur.jpg"),
-            const SizedBox(
-              height: 64,
-            ),  
-          //circular widget pour accepter ete montrer le file selector pour l'avatar
-          Stack(
-            children: [
-              CircleAvatar(
-                radius: 64,
-                backgroundImage: NetworkImage("https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"),
-
-              ),
-            ],
-          ),
-             const SizedBox(
-              height: 24,
-            ),  
-             //text field input pour le nom d'utilisateur
-              TextFieldInput(
-              hinText: "Entrez votre Nom d'utilisateur ",
-              textInputType: TextInputType.emailAddress,
-              textEditingController: _usernameController,
-            ),
-    
-            const SizedBox(
-              height: 24,
-            ),
-          
-            //Text field input pour l'email
-            TextFieldInput(
-              hinText: "Entrez votre email ",
-              textInputType: TextInputType.emailAddress,
-              textEditingController: _emailController,
-            ),
-            //text field input pour le mot de passe
-            const SizedBox(
-              height: 24,
-            ),
-            TextFieldInput(
-              hinText: "Entrez votre Mot de passe ",
-              textInputType: TextInputType.text,
-              textEditingController: _passwordController,
-              isPass: true,
-            ),
-               const SizedBox(
-              height: 24,
-            ),
-        
-                //text field input pour la bio
-              TextFieldInput(
-              hinText: "Entrez votre Bio ",
-              textInputType: TextInputType.emailAddress,
-              textEditingController: _bioController,
-            ),
-    
-            const SizedBox(
-              height: 24,
-            ),
-        
-            //bouton login
-            Container(
-              child: Text(
-                "Inscrivez-vous",
-                style: GoogleFonts.poppins(
-                  fontSize: 19,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              width: double.infinity,
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: const ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4))),
-                color: redColor,
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-
-            Flexible(
-              child: Container(),
-              flex: 2,
-            ),
-            //transition vers signing up
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text(
-                    "déjà un compte ?",
-                    style: GoogleFonts.poppins(),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                GestureDetector(
-                  onTap: () {
-                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const LoginScreen()));
-                  },
-                  child: Container(
-                    child: Text(
-                      "Connectez-vous !",
-                      style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.bold, color: redColor),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: SizedBox(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 10,
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                ),
-              ],
+                    // image
+                    Image.asset("image/observateur.jpg"),
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    //circular widget pour accepter ete montrer le file selector pour l'avatar
+                    Stack(
+                      children: [
+                       _image != null ? CircleAvatar(
+                          radius: 64,
+                          backgroundImage: MemoryImage(_image!),
+                        ) : const CircleAvatar(
+                          radius: 64,
+                          backgroundImage: NetworkImage(
+                              "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg"),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          left: 80,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: IconButton(
+                              onPressed: selectImage,
+                              icon: const Icon(Icons.add_a_photo),
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    //text field input pour le nom d'utilisateur
+                    TextFieldInput(
+                      hinText: "Entrez votre Nom d'utilisateur ",
+                      textInputType: TextInputType.emailAddress,
+                      textEditingController: _usernameController,
+                    ),
+
+                    const SizedBox(
+                      height: 24,
+                    ),
+
+                    //Text field input pour l'email
+                    TextFieldInput(
+                      hinText: "Entrez votre email ",
+                      textInputType: TextInputType.emailAddress,
+                      textEditingController: _emailController,
+                    ),
+
+                    //text field input pour le mot de passe
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    TextFieldInput(
+                      hinText: "Entrez votre Mot de passe ",
+                      textInputType: TextInputType.text,
+                      textEditingController: _passwordController,
+                      isPass: true,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+
+                    //text field input pour la bio
+                    TextFieldInput(
+                      hinText: "Entrez votre Bio ",
+                      textInputType: TextInputType.emailAddress,
+                      textEditingController: _bioController,
+                    ),
+
+                    const SizedBox(
+                      height: 24,
+                    ),
+
+                    //bouton login
+                    InkWell(
+                      onTap: () async {
+                        String res = await AuthMethods().signUpUser(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                          username: _usernameController.text,
+                          bio: _bioController.text,
+                          file: _image!,
+                        );
+
+                        // ignore: avoid_print
+                        print(res);
+                      },
+                      child: Container(
+                        child: Text(
+                          "Inscrivez-vous",
+                          style: GoogleFonts.poppins(
+                            fontSize: 19,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        decoration: const ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4))),
+                          color: redColor,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+
+                    //transition vers signing up
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text(
+                            "déjà un compte ?",
+                            style: GoogleFonts.poppins(),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginScreen()));
+                          },
+                          child: Container(
+                            child: Text(
+                              "Connectez-vous !",
+                              style: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.bold, color: redColor),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]),
             ),
-          ]),
+          ),
         ),
       ),
     );
