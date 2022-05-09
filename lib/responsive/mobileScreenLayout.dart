7dc9ed91bff4682388967fe6d659492d1b44e642
wassimouricha <wbouricha5@gmail.com>
2022-05-09 15:06:27 +0200

    mobile screen layout modified

--- lib/responsive/mobileScreenLayout.dart
+++ lib/responsive/mobileScreenLayout.dart
@@ -1,16 +1,46 @@
 // ignore_for_file: file_names
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
-  const MobileScreenLayout({ Key? key }) : super(key: key);
+class MobileScreenLayout extends StatefulWidget {
+  const MobileScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String username = "";
+
+  @override
+  void initState() {
+    // implémentation d'initState
+    super.initState();
+    getUsername();
+  }
+
+  void getUsername() async {
+    DocumentSnapshot snap = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    setState(() {
+      username = (snap.data() as Map<String, dynamic>) ["username"];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(child: Text("L'observateur",  style: TextStyle(color: Colors.black),),
-        
+      body: Center(
+        child: Text(
+          "L'observateur",
+          style: TextStyle(color: Colors.black),
         ),
+      ),
     );
   }
 }
